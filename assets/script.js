78fe00a9746161ad5341aee9ede02bea1a775b6a
parson40 <101997352+parsonjd@users.noreply.github.com>
2022-08-08 15:50:40 -0400

    Provided functionality to saving recent city searches on the page, provided local storage for persistent data, and added try/catch to my api calls

--- assets/script.js
+++ assets/script.js
@@ -3,30 +3,82 @@ formSubmit = document.querySelector("#search-city");
 currentCity = document.querySelector("#current-city");
 currentWeather = document.querySelector("#current-weather");
 fiveDay = document.querySelector("#five-day");
+recentSearchCont = document.querySelector("#search-buttons");
+error = document.querySelector("#error");
 
-
-//My API Key
+//My API Key & cities variable for local storage
 let apiKey = "b25ca6f3c40159f7eb3124b1a145f1bc"
+let cities = [];
 
 //Accepts city from user input and pushes it cityWeather to get weather data from API
+//Passes off to other functions to get five day forecast, save the city search buttons, store the city in local storage, and then clears input form 
 function handleForm(e) {
     e.preventDefault();
     let cityName = citySearch.value.trim();
     if (cityName) {
         cityWeather(cityName);
         fiveDayOutlook(cityName);
+        recentSearch(cityName);
+        citySearch.value = "";
+        saveCity(cityName);
+        error.innerText = "";
+
     } else {
-        alert("Please include a city in your search!")
+        errorMessage();
     }
-    citySearch.value = "";
+
+}
+
+//Display error message if the city input is left blank
+function errorMessage() {
+    error.innerText = "Please enter a city name!";
+}
+
+//This is called upon opening the webpage or refresh to get items from local storage and display the stored cities as buttons on previously searched city area
+function init() {
+    let storedCities = JSON.parse(localStorage.getItem("cities"));
+
+    for (let city of storedCities) {
+
+        recentCity = document.createElement("button");
+        recentCity.innerText = city;
+        recentCity.classList = "w-100 btn-secondary p-2 my-2 recent-button";
+        recentCity.setAttribute("data-type", city)
+        recentCity.setAttribute("type", "submit");
+
+        recentSearchCont.prepend(recentCity);
+    }
+}
+
+//Save each searched city in local storage
+function saveCity(city) {
+    cities.unshift(city);
+    localStorage.setItem("cities", JSON.stringify(cities));
+
+}
+
+function recentSearch(city) {
+
+    recentCity = document.createElement("button");
+    recentCity.innerText = city;
+    recentCity.classList = "w-100 btn-secondary p-2 my-2 recent-button";
+    recentCity.setAttribute("data-type", city)
+    recentCity.setAttribute("type", "submit");
+
+    recentSearchCont.prepend(recentCity);
+
 }
 
 //Fetches weather data from API and passes off to display on the page
 async function cityWeather(city) {
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`);
-    let data = await response.json();
-    renderWeather(data, city);
-
+    try {
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`);
+        let data = await response.json();
+        renderWeather(data, city);
+    }
+    catch (e) {
+        console.log("Error", e);
+    }
 }
 
 //Displays the weather data
@@ -40,11 +92,15 @@ function renderWeather(data, city) {
     currentCity.append(date);
 
     //Display the weather ICON
-    let icon = document.createElement("img");
-    icon.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
-    icon.setAttribute('height', "50px");
-    currentCity.append(icon);
-
+    try {
+        let icon = document.createElement("img");
+        icon.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+        icon.setAttribute('height', "50px");
+        currentCity.append(icon);
+    }
+    catch (e) {
+        console.log("Error", e);
+    }
     //Display current temperature
     let currentTemp = document.createElement("p");
     currentTemp.innerText = `Temp: ${data.main.temp} \u2109`;
@@ -68,9 +124,14 @@ function renderWeather(data, city) {
 
 //Fetch UV data from API and pass to function to display
 async function uvIndex(latitude, longitude) {
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/uvi?lat=${latitude}&lon=${longitude}&appid=${apiKey}`);
-    let data = await response.json();
-    renderUVIndex(data);
+    try {
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/uvi?lat=${latitude}&lon=${longitude}&appid=${apiKey}`);
+        let data = await response.json();
+        renderUVIndex(data);
+    }
+    catch (e) {
+        console.log("Error", e);
+    }
 }
 
 //Displays UV index on page
@@ -95,9 +156,14 @@ function renderUVIndex(UV) {
 
 //Get the five-day outlook from the API
 async function fiveDayOutlook(city) {
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`);
-    let data = await response.json();
-    renderFiveDay(data);
+    try {
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`);
+        let data = await response.json();
+        renderFiveDay(data);
+    }
+    catch (e) {
+        console.log("Error", e);
+    }
 }
 
 //Display five-day forecast
@@ -106,7 +172,6 @@ function renderFiveDay(days) {
     //Increment by eight so the eight time hacks of each day are skipped over so we can get a five-day outlook
     for (let i = 1; i < days.list.length; i += 8) {
         let dayForecast = days.list[i];
-        console.log(dayForecast);
 
         //Create div to hold each day's forecast - keeping each day a column width of 2
         let fiveDayForecast = document.createElement("div");
@@ -119,12 +184,16 @@ function renderFiveDay(days) {
         fiveDay.append(fiveDayForecast);
 
         //Create and display daily weather icon
-        let dailyIcon = document.createElement("img");
-        dailyIcon.classList = "text-center";
-        dailyIcon.setAttribute("src", `https://openweathermap.org/img/wn/${dayForecast.weather[0].icon}@2x.png`);
-        dailyIcon.setAttribute('height', "30px");
-        fiveDayForecast.append(dailyIcon);
-
+        try {
+            let dailyIcon = document.createElement("img");
+            dailyIcon.classList = "text-center";
+            dailyIcon.setAttribute("src", `https://openweathermap.org/img/wn/${dayForecast.weather[0].icon}@2x.png`);
+            dailyIcon.setAttribute('height', "30px");
+            fiveDayForecast.append(dailyIcon);
+        }
+        catch (e) {
+            console.log("Error", e);
+        }
         //Create and display daily temperature
         let dailyTemp = document.createElement("p");
         dailyTemp.classList = "text-center";
@@ -146,5 +215,15 @@ function renderFiveDay(days) {
     }
 }
 
-//Handles form submission
+function recentSearchHandle(e) {
+    let city = e.target.getAttribute("data-type");
+    cityWeather(city);
+    fiveDayOutlook(city);
+}
+
+//Call the init function upon load or refresh
+init();
+
+//Handles form submission or recent city search button clicks
 formSubmit.addEventListener("submit", handleForm);
+recentSearchCont.addEventListener("click", recentSearchHandle)
